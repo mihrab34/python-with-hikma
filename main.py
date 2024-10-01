214d0631ea98e9d7816e2f9ef47ee0e401a85fee
Akintola Rahmat <akintolarahmah@gmail.com>
2024-10-01 22:32:22 +0000

    learnt tuples

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
 # # Main Data Types
 #
-firstname: str = "Rahmat"
-lastname = "Akintola"
-
-fullname = f"{firstname} Olajumoke {lastname}"  # string interpolation with f-strings
-print(fullname)
+# firstname: str = "Rahmat"
+# lastname = "Akintola"
 #
-price = 50
-
-print(f'{price} GHS')
-
-total = 30
+# fullname = f"{firstname} Olajumoke {lastname}"  # string interpolation with f-strings
+# print(fullname)
 #
-prices = '50'
-
-print(int(prices) + total)  # type conversion
-
-print(total // 10)
-
-print(type(total))
+# price = 50
+#
+# print(f'{price} GHS')
+#
+# total = 30
+# #
+# prices = '50'
+#
+# print(int(prices) + total)  # type conversion
+#
+# print(total // 10)
+#
+# print(type(total))
 
 # Lists
-names = ["sola", 89, True, {"message": "Form created"}]
-
-print(names)
+# names = ["sola", 89, True, {"message": "Form created"}]
 #
-names[0] = "Hikma"
-
-names.append(24)
-names.pop()
-print(len(names))
-names.insert(3, "Demo")
-print(names)
+# print(names)
+#
+# names[0] = "Hikma"
+#
+# names.append(24)
+# names.pop()
+# print(len(names))
+# names.insert(3, "Demo")
+# print(names)
 
 # Dictionary
 
-fruits = {"name": "orange", "quantity": 20, "status": False}
+# fruits = {"name": "orange", "quantity": 20, "status": False}
+#
+# fruits['name'] = "Apple"
+# print(f" I have {fruits['quantity']} {fruits['name']}s")
+# fruits.pop("status")
+# print(fruits)
 
-fruits['name'] = "Apple"
-print(f" I have {fruits['quantity']} {fruits['name']}s")
-fruits.pop("status")
-print(fruits)
+# Tuples
+
+my_fruits = ('banana', 'apple', 'cherry')
+post_ids = 12345, 54321, 'hello!'
+
+print(my_fruits[1])
+# nesting tuples
+post_fruits = my_fruits, post_ids, (1, 2, 3, 4, 5)
+
+print(post_fruits)  # (('banana', 'apple', 'cherry'), (12345, 54321, 'hello!'), (1, 2, 3, 4, 5))
+print(post_fruits[1][1])  # 54321
+
+
+
